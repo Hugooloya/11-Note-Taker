c8f433482fd0a8efab63fb70eac6f4a30c30bb4c
Hugo <hugooloya@gmail.com>
2022-12-07 17:21:44 -0700

    Routes, Helpers added
    Added all the functional routing and helpers.
    Deleting notes still in working progress

---
+++ routes/notes.js
@@ -0,0 +1,40 @@
+const notes = require("express").Router();
+const { json } = require("express");
+const {
+  readFromFile,
+  readAndAppend,
+  readAndDelete,
+} = require("../helpers/fsUtils");
+const { v4: uuidv4 } = require('uuid');
+
+// GET method that reads db file and returns it
+notes.get("/", (req, res) => 
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
+);
+
+// POST method that reads db file, add new note and appends to the file.
+notes.post("/", (req, res) => {
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Note added successfully`);
+
+  } else {
+    res.status(500).json("Error saving note.");
+  }
+});
+
+// notes.delete("/:id", (req, res) => {
+//   readAndDelete("./db/db.json", req.params.id);
+//   res.json(`Note deleted successfully`);
+// });
+
+module.exports = notes;
